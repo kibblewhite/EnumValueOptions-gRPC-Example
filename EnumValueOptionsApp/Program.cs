8cafcf9baad751a43cc339675e84bbfd3d586fc5
@kibblewhite <kibblewhite@live.com>
2021-12-01 13:13:10 +0100

    Initial checkin of code

---
+++ EnumValueOptionsApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Google.Protobuf.Reflection;
+using Options.Common;
+
+Console.WriteLine("Hello, World!");
+
+EnumDescriptor SystemRoleTypeTypeEnumDescriptor = ProfilesEnumReflection.Descriptor.FindTypeByName<EnumDescriptor>(typeof(ProfilesEnum).Name);
+foreach (ProfilesEnum system_role_type in Enum.GetValues(typeof(ProfilesEnum)))
+{
+    EnumValueDescriptor enum_value_descriptor = SystemRoleTypeTypeEnumDescriptor.FindValueByNumber((int)system_role_type);
+    var selector_value = enum_value_descriptor.GetOptions().GetExtension<string>(EnumValueOptionsExtensions.EnumName);
+    Console.WriteLine(selector_value);
+}
+
+Console.ReadLine();
